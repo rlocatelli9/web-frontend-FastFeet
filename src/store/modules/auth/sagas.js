5ca7803dce6d877f681f82172c6c5a22b17630ae
Robson Locatelli <rlocatelli9@gmail.com>
2020-04-05 18:18:54 -0300

    feat: added store

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,31 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+
+import history from '../../../services/history';
+import api from '../../../services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (!user) {
+      console.tron.error('Usuário não encontrado');
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
